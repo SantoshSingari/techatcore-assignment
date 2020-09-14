77da55c9b476b3d7a0fa688d2562909140404f9c
Vivek <60694023+VivekTej99@users.noreply.github.com>
2020-09-14 23:36:40 +0530

    Careers Page Our team Section Updated (#4)
    * Added a navigation-bar, Banner and it's content with a scroll down button
    
    * Created a LandingPage with all sections except success stories slideshow section and also created careers page with two sections
    
    * Completed all sections of Carrers Page and left with only Success stories section in Landing page
    
    * added the images in our team section
    
    * Updated Readme.md
    
    * Updated author in Package.json
    
    Changed author field to tech.at.core
    
    * updated author in meta tag

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# t-website
-techatcore website
+# tac-website
+static website for tech.at.core
+
+___
+
+## 🚀Prerequisites
+* Node.js
+* gatsby cli: `npm i -g gatsby-cli`
+* typescript: `npm i -g gatsby-plugin-typescript`
+
+## Setup
+* clone this repo
+* `cd tac-website`
+* npm install
+
+
+## How to run
+* `gatsby develop`
+
+
+___
+
+
+## ❤️ Credits
+Built with [Gatsby](https://www.gatsbyjs.org/) - the blazing-fast static site generator for [React](https://facebook.github.io/react/).
+
+## 💫 Deploy
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/resir014/gatsby-starter-typescript-plus)
+--
+
+___
+
---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `techatcore Website`,
+    description: `A website for working with any company Individuals for greater support from us.`,
+    author: `@tac`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `tech.at.core`,
+        short_name: `TAC`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
+      },
+    },
+    
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
